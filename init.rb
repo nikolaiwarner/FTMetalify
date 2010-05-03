0523b232d43b71db5c54d0f989e3264999f2fe12
nikolaiwarner <n@nikolaiwarner.com>
2010-05-03 18:23:36 -0400

    first commit

---
+++ init.rb
@@ -0,0 +1,13 @@
+ViewExtender.register(
+  'dashboard/javascripts',
+  :bottom,
+  'dashboard/javascripts/rh_metalify_scripts',
+  :partial => 'scripts/metalify'
+)
+
+ViewExtender.register(
+  'dashboard/stylesheets',
+  :bottom,
+  'dashboard/stylesheets/rh_metalify_scripts',
+  :partial => 'styles/metalify'
+)
---
+++ tasks/rh_metalify_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :rh_metalify do
+#   # Task goes here
+# end
